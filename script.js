f3a43f8b278d27a9cff2e0a6755bc35418d351ab
mennaibrahim23 <mennaibrahim230@gmail.com>
2026-08-06 19:20:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+// إزالة شاشة البداية بعد ثانيتين
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+    }, 2000);
+});
 
+// زر فتح الدعوة
+document.getElementById("openInvitation").addEventListener("click", () => {
+    document.getElementById("invitation").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+// Countdown
+const weddingDate = new Date("September 6, 2026 18:00:00").getTime();
+
+setInterval(() => {
+
+    const now = new Date().getTime();
+
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (distance % (1000 * 60))
+        / 1000
+    );
+
+    document.getElementById("countdown").innerHTML =
+        `${days} Days<br>${hours} Hours<br>${minutes} Minutes<br>${seconds} Seconds`;
+
+},1000);
